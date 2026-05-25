8da97ffc56a1aba03552d7fbbad59b7b78341db8
sevneagle <sevneagle@gmail.com>
2026-05-25 23:53:35 +0800

    urlchange

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.siteURL,
+	site: 'https://patchouli.group',
 	base: "/",
 	trailingSlash: "always",
 
