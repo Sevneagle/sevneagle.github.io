af0f68ad97640a9e543fc147121b24370133adaf
sevneagle <sevneagle@gmail.com>
2026-09-22 20:25:23 +0800

    域名更新

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "七鹰",
 	subtitle: "Operation",
-	siteURL: "https://patchouli.group/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://patchouli.tech/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-12-30", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
